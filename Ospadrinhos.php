ff88fd438cbc10b11c7c6c13ac5c3968b6d7ca82
Ramon Barros <ramonfilipe@gmail.com>
2019-06-02 22:27:57 -0300

    atualização da pagina padrinhos

--- Ospadrinhos.php
+++ Ospadrinhos.php
@@ -44,6 +44,116 @@
                   <p> Endereço: Av. Getúlio Vargas, s/n - Monte Castelo, São Luís - MA, 65030-005</p>
                 </div>
             </li>
+
+          <li class="row">
+            <figure class="col-sm-12 col-md-3">
+              <img class="card-img" src="img/pic22.jpg" alt="Rikelma Baldez">
+            </figure>
+
+            <div class="col-sm-12 col-md-7">
+                <h3> Rikelma Baldez</h3>
+                <p> Endereço: Av. dos Holandeses, S/N - Lote 16 - Calhau, São Luís - MA, 65071-380  </p>
+              </div>
+          </li>
+          <div style="height: 25px"></div>
+
+          <li class="row">
+            <figure class="col-sm-12 col-md-3">
+              <img class="card-img" src="img/pic23.jpg" alt="Deimoselles -  Nicole">
+            </figure>
+
+            <div class="col-sm-12 col-md-7">
+                <h3> Deimoselles -  Nicole</h3>
+                <p> Endereço: Av. Getúlio Vargas, s/n - Monte Castelo, São Luís - MA, 65030-005</p>
+              </div>
+          </li>
+          
+          <div style="height: 25px"></div>
+
+            <li class="row">
+              <figure class="col-sm-12 col-md-3">
+                <img class="card-img" src="img/pic24.jpg" alt="Marcos Braga">
+              </figure>
+
+              <div class="col-sm-12 col-md-7">
+                  <h3> Marcos Braga</h3>
+                  <p> Endereço: Av. Getúlio Vargas, s/n - Monte Castelo, São Luís - MA, 65030-005</p>
+                </div>
+            </li>
+
+            <li class="row">
+            <figure class="col-sm-12 col-md-3">
+              <img class="card-img" src="img/pic25.jpg" alt="Rikelma Baldez">
+            </figure>
+
+            <div class="col-sm-12 col-md-7">
+                <h3> Rikelma Baldez</h3>
+                <p> Endereço: Av. dos Holandeses, S/N - Lote 16 - Calhau, São Luís - MA, 65071-380  </p>
+              </div>
+          </li>
+          <div style="height: 25px"></div>
+
+          <li class="row">
+            <figure class="col-sm-12 col-md-3">
+              <img class="card-img" src="img/pic26.jpg" alt="Deimoselles -  Nicole">
+            </figure>
+
+            <div class="col-sm-12 col-md-7">
+                <h3> Deimoselles -  Nicole</h3>
+                <p> Endereço: Av. Getúlio Vargas, s/n - Monte Castelo, São Luís - MA, 65030-005</p>
+              </div>
+          </li>
+          
+          <div style="height: 25px"></div>
+
+            <li class="row">
+              <figure class="col-sm-12 col-md-3">
+                <img class="card-img" src="img/pic27.jpg" alt="Marcos Braga">
+              </figure>
+
+              <div class="col-sm-12 col-md-7">
+                  <h3> Marcos Braga</h3>
+                  <p> Endereço: Av. Getúlio Vargas, s/n - Monte Castelo, São Luís - MA, 65030-005</p>
+                </div>
+            </li>
+
+            <li class="row">
+            <figure class="col-sm-12 col-md-3">
+              <img class="card-img" src="img/pic28.jpg" alt="Rikelma Baldez">
+            </figure>
+
+            <div class="col-sm-12 col-md-7">
+                <h3> Rikelma Baldez</h3>
+                <p> Endereço: Av. dos Holandeses, S/N - Lote 16 - Calhau, São Luís - MA, 65071-380  </p>
+              </div>
+          </li>
+          <div style="height: 25px"></div>
+
+          <li class="row">
+            <figure class="col-sm-12 col-md-3">
+              <img class="card-img" src="img/pic29.jpg" alt="Deimoselles -  Nicole">
+            </figure>
+
+            <div class="col-sm-12 col-md-7">
+                <h3> Deimoselles -  Nicole</h3>
+                <p> Endereço: Av. Getúlio Vargas, s/n - Monte Castelo, São Luís - MA, 65030-005</p>
+              </div>
+          </li>
+          
+          <div style="height: 25px"></div>
+
+            <li class="row">
+              <figure class="col-sm-12 col-md-3">
+                <img class="card-img" src="img/pic30.jpg" alt="Marcos Braga">
+              </figure>
+
+              <div class="col-sm-12 col-md-7">
+                  <h3> Marcos Braga</h3>
+                  <p> Endereço: Av. Getúlio Vargas, s/n - Monte Castelo, São Luís - MA, 65030-005</p>
+                </div>
+            </li>
+
+            
 </ul>
 
 <?php include_once("footer.php");?>
