03e50bd42d13b208d042a4e8e9ff51143c2fc65a
Ramon Barros <ramonfilipe@gmail.com>
2019-06-07 11:34:20 -0300

    modificação na pagina padrinhos

--- Ospadrinhos.php
+++ Ospadrinhos.php
@@ -6,154 +6,250 @@
   </div>
 </header>
 
-<!-- Page Content -->
 
-<ul id="formatado">
-          <li class="row">
-            <figure class="col-sm-12 col-md-3">
+
+
+
+
+
+
+  <!-- /1 primeira row -->
+ 
+
+
+
+
+
+
+
+  <div class="container">
+  <div class="row">
+    <!-- Team Member 1 -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+      <figure class="col-sm-6  col-md-12">
               <img class="card-img" src="img/pic19.jpg" alt="Rikelma Baldez">
             </figure>
-
-            <div class="col-sm-12 col-md-7">
-                <h3> Rikelma Baldez</h3>
-                <p> Endereço: Av. dos Holandeses, S/N - Lote 16 - Calhau, São Luís - MA, 65071-380  </p>
-              </div>
-          </li>
-          <div style="height: 25px"></div>
-
-          <li class="row">
-            <figure class="col-sm-12 col-md-3">
+        <div class="card-body text-center">
+          <h5 class="card-title mb-0">Rikelma Baldez</h5>
+          <div class="card-text text-black-50"></div>
+        </div>
+      </div>
+    </div>
+    <!-- Team Member 2 -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+      <figure class="col-sm-12 col-md-12">
               <img class="card-img" src="img/pic20.jpg" alt="Deimoselles -  Nicole">
             </figure>
-
-            <div class="col-sm-12 col-md-7">
-                <h3> Deimoselles -  Nicole</h3>
-                <p> Endereço: Av. Getúlio Vargas, s/n - Monte Castelo, São Luís - MA, 65030-005</p>
-              </div>
-          </li>
-          
-          <div style="height: 25px"></div>
-
-            <li class="row">
-              <figure class="col-sm-12 col-md-3">
-                <img class="card-img" src="img/pic21.jpg" alt="Marcos Braga">
-              </figure>
-
-              <div class="col-sm-12 col-md-7">
-                  <h3> Marcos Braga</h3>
-                  <p> Endereço: Av. Getúlio Vargas, s/n - Monte Castelo, São Luís - MA, 65030-005</p>
-                </div>
-            </li>
-
-          <li class="row">
-            <figure class="col-sm-12 col-md-3">
-              <img class="card-img" src="img/pic22.jpg" alt="Rikelma Baldez">
+        <div class="card-body text-center">
+          <h5 class="card-title mb-0">Marcos Braga</h5>
+          <div class="card-text text-black-50"></div>
+        </div>
+      </div>
+    </div>
+    <!-- Team Member 3 -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+            <figure class="col-sm-12 col-md-12">
+              <img class="card-img" src="img/pic21.jpg" alt="Deimoselles -  Nicole">
             </figure>
+        <div class="card-body text-center">
+          <h5 class="card-title mb-0">Tia Bela</h5>
+          <div class="card-text text-black-50"></div>
+        </div>
+      </div>
+    </div>
+    <!-- Team Member 4 -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+      <figure class="col-sm-12 col-md-12">
+              <img class="card-img" src="img/pic22.jpg" alt="Deimoselles -  Nicole">
+            </figure>
+        <div class="card-body text-center">
+          <h5 class="card-title mb-0">Tio Hélio</h5>
+          <div class="card-text text-black-50"></div>
+        </div>
+      </div>
+    </div>
 
-            <div class="col-sm-12 col-md-7">
-                <h3> Rikelma Baldez</h3>
-                <p> Endereço: Av. dos Holandeses, S/N - Lote 16 - Calhau, São Luís - MA, 65071-380  </p>
-              </div>
-          </li>
-          <div style="height: 25px"></div>
 
-          <li class="row">
-            <figure class="col-sm-12 col-md-3">
+  <!-- / 2 segunda row -->
+  <div class="container">
+  <div class="row">
+    <!-- Team Member 1 -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+        <figure class="col-sm-6  col-md-12">
+              <img class="card-img" src="img/pic01.png" alt="Rikelma Baldez">
+            </figure>
+        <div class="card-body text-center">
+          <h5 class="card-title mb-0">Camila e Mário Jr</h5>
+          <div class="card-text text-black-50"></div>
+        </div>
+      </div>
+    </div>
+    <!-- Team Member 2 -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+            <figure class="col-sm-12 col-md-12">
               <img class="card-img" src="img/pic23.jpg" alt="Deimoselles -  Nicole">
             </figure>
-
-            <div class="col-sm-12 col-md-7">
-                <h3> Deimoselles -  Nicole</h3>
-                <p> Endereço: Av. Getúlio Vargas, s/n - Monte Castelo, São Luís - MA, 65030-005</p>
-              </div>
-          </li>
-          
-          <div style="height: 25px"></div>
-
-            <li class="row">
-              <figure class="col-sm-12 col-md-3">
-                <img class="card-img" src="img/pic24.jpg" alt="Marcos Braga">
-              </figure>
-
-              <div class="col-sm-12 col-md-7">
-                  <h3> Marcos Braga</h3>
-                  <p> Endereço: Av. Getúlio Vargas, s/n - Monte Castelo, São Luís - MA, 65030-005</p>
-                </div>
-            </li>
-
-            <li class="row">
-            <figure class="col-sm-12 col-md-3">
-              <img class="card-img" src="img/pic25.jpg" alt="Rikelma Baldez">
+        <div class="card-body text-center">
+          <h5 class="card-title mb-0">Crenilde e Haroldo</h5>
+          <div class="card-text text-black-50"></div>
+        </div>
+      </div>
+    </div>
+    <!-- Team Member 3 -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+            <figure class="col-sm-12 col-md-12">
+              <img class="card-img" src="img/pic24.jpg" alt="Deimoselles -  Nicole">
             </figure>
-
-            <div class="col-sm-12 col-md-7">
-                <h3> Rikelma Baldez</h3>
-                <p> Endereço: Av. dos Holandeses, S/N - Lote 16 - Calhau, São Luís - MA, 65071-380  </p>
-              </div>
-          </li>
-          <div style="height: 25px"></div>
-
-          <li class="row">
-            <figure class="col-sm-12 col-md-3">
-              <img class="card-img" src="img/pic26.jpg" alt="Deimoselles -  Nicole">
+        <div class="card-body text-center">
+          <h5 class="card-title mb-0">Alicia e Julio Aderson</h5>
+          <div class="card-text text-black-50"></div>
+        </div>
+      </div>
+    </div>
+    <!-- Team Member 4 -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+            <figure class="col-sm-12 col-md-12">
+              <img class="card-img" src="img/pic25.jpg" alt="Deimoselles -  Nicole">
             </figure>
-
-            <div class="col-sm-12 col-md-7">
-                <h3> Deimoselles -  Nicole</h3>
-                <p> Endereço: Av. Getúlio Vargas, s/n - Monte Castelo, São Luís - MA, 65030-005</p>
-              </div>
-          </li>
-          
-          <div style="height: 25px"></div>
-
-            <li class="row">
-              <figure class="col-sm-12 col-md-3">
-                <img class="card-img" src="img/pic27.jpg" alt="Marcos Braga">
-              </figure>
-
-              <div class="col-sm-12 col-md-7">
-                  <h3> Marcos Braga</h3>
-                  <p> Endereço: Av. Getúlio Vargas, s/n - Monte Castelo, São Luís - MA, 65030-005</p>
-                </div>
-            </li>
-
-            <li class="row">
-            <figure class="col-sm-12 col-md-3">
-              <img class="card-img" src="img/pic28.jpg" alt="Rikelma Baldez">
+        <div class="card-body text-center">
+          <h5 class="card-title mb-0">Rarissa e Daniel</h5>
+          <div class="card-text text-black-50"></div>
+        </div>
+      </div>
+    </div>
+</div>
+     <!-- / 3  row -->
+  <div class="row">
+    <!-- Team Member 1 -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+        <figure class="col-sm-6  col-md-12">
+              <img class="card-img" src="img/pic26.jpg" alt="Rikelma Baldez">
             </figure>
+        <div class="card-body text-center">
+          <h5 class="card-title mb-0">Carol e Guilherme</h5>
+          <div class="card-text text-black-50"> </div>
+        </div>
+      </div>
+    </div>
+    
+    
 
-            <div class="col-sm-12 col-md-7">
-                <h3> Rikelma Baldez</h3>
-                <p> Endereço: Av. dos Holandeses, S/N - Lote 16 - Calhau, São Luís - MA, 65071-380  </p>
-              </div>
-          </li>
-          <div style="height: 25px"></div>
+     <!-- / 4  row -->
+  <div class="row">
+    <!-- Team Member 1 -->
+    
+    <!-- Team Member 2 -->
+    
+    <!-- Team Member 3 -->
+    
+    <!-- Team Member 4 -->
+    
 
-          <li class="row">
-            <figure class="col-sm-12 col-md-3">
-              <img class="card-img" src="img/pic29.jpg" alt="Deimoselles -  Nicole">
+
+
+<div class="container">
+    <h1 class="font-weight-light text-black text-center">Demoiselle</h1>
+</div>
+
+<br>
+<br>
+<div class="container">
+  <div class="row">
+    <!-- Team Member 1 -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+        <figure class="col-sm-6  col-md-12">
+              <img class="card-img" src="img/pic40.jpg" alt="Rikelma Baldez">
             </figure>
+        <div class="card-body text-center">
+          <h5 class="card-title mb-0">Sara Miranda</h5>
+          <div class="card-text text-black-50"> </div>
+        </div>
+      </div>
+    </div>
+    <!-- Team Member 2 -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+            <figure class="col-sm-12 col-md-12">
+              <img class="card-img" src="img/pic41.jpg" alt="Deimoselles -  Nicole">
+            </figure>
+        <div class="card-body text-center">
+          <h5 class="card-title mb-0">Nicole Costa</h5>
+          <div class="card-text text-black-50"></div>
+        </div>
+      </div>
+    </div>
+    <!-- Team Member 3 -->
+    
+    <!-- Team Member 4 -->
+    
+  <!-- /1 primeira row -->
 
-            <div class="col-sm-12 col-md-7">
-                <h3> Deimoselles -  Nicole</h3>
-                <p> Endereço: Av. Getúlio Vargas, s/n - Monte Castelo, São Luís - MA, 65030-005</p>
-              </div>
-          </li>
-          
-          <div style="height: 25px"></div>
+  <div class="container">
+    <h1 class="font-weight-light text-black text-center">Daminha e Pajens</h1>
+</div>
 
-            <li class="row">
-              <figure class="col-sm-12 col-md-3">
-                <img class="card-img" src="img/pic30.jpg" alt="Marcos Braga">
-              </figure>
-
-              <div class="col-sm-12 col-md-7">
-                  <h3> Marcos Braga</h3>
-                  <p> Endereço: Av. Getúlio Vargas, s/n - Monte Castelo, São Luís - MA, 65030-005</p>
-                </div>
-            </li>
-
-            
-</ul>
+<br>
+<br>
+<div class="container">
+  <div class="row">
+    <!-- Team Member 1 -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+        <figure class="col-sm-6  col-md-12">
+              <img class="card-img" src="img/pic42.jpg" alt="Rikelma Baldez">
+            </figure>
+        <div class="card-body text-center">
+          <h5 class="card-title mb-0">Bianca</h5>
+          <div class="card-text text-black-50"> </div>
+        </div>
+      </div>
+    </div>
+    <!-- Team Member 2 -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+            <figure class="col-sm-12 col-md-12">
+              <img class="card-img" src="img/pic43.jpg" alt="Deimoselles -  Nicole">
+            </figure>
+        <div class="card-body text-center">
+          <h5 class="card-title mb-0">João Francisco</h5>
+          <div class="card-text text-black-50"></div>
+        </div>
+      </div>
+    </div>
+    <!-- Team Member 3 -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+            <figure class="col-sm-12 col-md-12">
+              <img class="card-img" src="img/pic44.jpg" alt="Deimoselles -  Nicole">
+            </figure>
+        <div class="card-body text-center">
+          <h5 class="card-title mb-0">Davi</h5>
+          <div class="card-text text-black-50"></div>
+        </div>
+      </div>
+    </div>
+    <!-- Team Member 4 -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+            <figure class="col-sm-12 col-md-12">
+              <img class="card-img" src="img/pic45.jpg" alt="Deimoselles -  Nicole">
+            </figure>
+        <div class="card-body text-center">
+          <h5 class="card-title mb-0">Miguel</h5>
+          <div class="card-text text-black-50"></div>
+        </div>
+      </div>
+    </div>
+  <!-- /1 primeira row -->
 
 <?php include_once("footer.php");?>
