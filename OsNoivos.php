c5f69c236f2886cccf4fa8a1357ba1b06d0e9a40
Ramon Barros <ramonfilipe@gmail.com>
2019-06-08 22:59:34 -0300

    fontes e card modificados

--- OsNoivos.php
+++ OsNoivos.php
@@ -7,17 +7,17 @@
     background-size: cover;
     -o-background-size: cover;">
 <div class="container">
-
-<h1 class="font-weight-light text-center text-lg-center mt-4 mb-0">Nossa história</h1>
-
-<h1 class="font-weight-light text-center text-lg-center mt-4 mb-0">Como é bom recordar!</h1>
+<div id="letra">
+<h2 class="font-weight-light text-center text-lg-center mt-4 mb-0">Nossa história</h2></div>
+<div class="container" id="letra">
+<h1 class="font-weight-light text-center text-lg-center mt-4 mb-0">Como é bom recordar!</h1></div>
 <div class="container">
   <p class="lead"> Conheça os detalhes da nossa caminhada até esse grande sonho do nosso casamento. <p class="lead">
   Em 2010 nossa história começou... Em pensar que Fernando iria encontrar o amor da sua vida em uma loja de vendas de motocicletas. Lá estava Larissa, que no ambiente do seu trabalho não cogitaria se relacionar com um cliente.<br>
   Eis que Deus conforme a sua vontade, fez o que poderia ser impossível acontecer.<p class="lead">
   De lá pra cá já vivemos muitos grandes momentos: viagens; baladas; oração e descanso. Também tivemos desencontros, choros e reecontros. Hoje! A certeza que o Amor é Maior que Tudo.<p class="lead">
-  Podemos dizer que a maior promessa do amor é o casamento. Aqui é que a felicidade pretende se eternizar e fazer morada. É no amor que reconhecemos que nós dois somos um.<p class="lead text-lg-center">
-  Larissa e Fernando 
+  Podemos dizer que a maior promessa do amor é o casamento. Aqui é que a felicidade pretende se eternizar e fazer morada. É no amor que reconhecemos que nós dois somos um.
+  <p class="lead text-lg-center">Larissa e Fernando</p> 
   
   </div>
 
