cc2c9eeea4f05a68652896aff3f5459db852aaf6
Ramon Barros <ramonfilipe@gmail.com>
2019-06-13 15:49:09 -0300

    conexão com banco de dados correto

--- conexao.php
+++ conexao.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = new PDO("mysql:dbname=larissaefernando_db; host=localhost", "root", "");
+$conn = new PDO("mysql:dbname=larissaefernando_db; host=127.0.0.1", "larissaefernando_usuario", "jos@280812amon");
 
 
 ?>
